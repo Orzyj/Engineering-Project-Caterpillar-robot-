75c5c63c6db16c074a20158118ee711ec47c9b00
Krystian Wolniak <57046666+Orzyj@users.noreply.github.com>
2024-04-01 14:54:16 +0200

    Added regex to check ip and port

--- settingsDialog.py
+++ settingsDialog.py
@@ -1,39 +1,56 @@
 from PyQt6.QtWidgets import QDialog
 from settingsDialog_ui import * 
+import re
 
 class SettingsDialog(QDialog):
     def __init__(self, ip: str, port: int):
         super().__init__()
         self.ui: Ui_Dialog = Ui_Dialog()
         self.ui.setupUi(self)
-        self._ip: str = ip
-        self._port: int = port
-        self._status: bool = False
+        self.__ip: str = ip
+        self.__port: int = port
+        self.__status: bool = False
 
-        self._initBaseConfiguration()
-        self.ui.buttonBox.accepted.connect(lambda: self._validateInput())
+        self.__initBaseConfiguration()
+        self.ui.buttonBox.accepted.connect(lambda: self.__validateInput())
+        self.ui.buttonBox.rejected.connect(lambda: self.reject())
 
-    def _initBaseConfiguration(self) -> None:
-        self.ui.leIP.setText(self._ip)
-        self.ui.lePort.setText(str(self._port))
+    def __checkIP(self, ip: str) -> bool:
+        pattern: str = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+        return True if re.match(pattern, ip) else False
+    
+    def __chekPort(self, port: str) -> bool:
+        try:
+            portNumber: int = int(port)
+            return True if isinstance(portNumber, int) and 0 < portNumber <= portNumber else False
+        except ValueError:
+            return False
+        
+    def __initBaseConfiguration(self) -> None:
+        self.ui.leIP.setText(self.__ip)
+        self.ui.lePort.setText(str(self.__port))
 
-    def _validateInput(self) -> None:
-        ip = self.ui.leIP.text()
-        port = self.ui.lePort.text()
+    def __validateInput(self) -> None:
+        ip: str = self.ui.leIP.text()
+        port: int = self.ui.lePort.text()
         
         if ip.strip() == "" or port.strip() == "":
-            self._status = False
-        else:
-            self._ip = self.ui.leIP.text()
-            self._port = int(self.ui.lePort.text())
-            self._status = True
+            self.__status = False
+
+        if self.__checkIP(ip) and self.__chekPort(port):
+            self.__ip = ip
+            self.__port = int(port)
+            self.__status = True
             self.accept() 
+        
+        else: 
+            self.__status = False
 
     def getIpAddress(self) -> str:
-        return self._ip
+        return self.__ip
     
     def getPort(self) -> int:
-        return self._port
+        return self.__port
     
     def getStatus(self) -> bool:
-        return self._status
+        return self.__status
